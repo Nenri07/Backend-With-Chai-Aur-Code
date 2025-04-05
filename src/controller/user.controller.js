a604451800154b5b3f4abf47298e89dccd0054fe
Nenri07 <umarbashir2002@gmail.com>
2025-04-06 02:55:55 +0500

    add channel details aggregation pipeline to fetch user channel info and subscription counts

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -36,6 +36,7 @@ import jwt from "jsonwebtoken"
 
 // }
 const generateAccessAndRefreshToken = async(userId) => {
+  
   try {
       const user = await User.findById(userId)
       if (!user) {
@@ -391,6 +392,78 @@ const updatCoverImage=asyncHandler(async(req,res)=>{
  
   
  })
+
+
+// Aggreagation pipelines for channel subs count
+
+const channelDetails=asyncHandler(async(req,res)=>{
+ const {username}= req.params
+ if (!username?.trim()) {
+  throw new apiError(404,"not found user channel")
+ }
+
+const channel= await User.aggregate([
+  {
+  $match:{
+    username:username?.toLowerCase()
+  }
+},
+{
+  $lookup:{
+    from:"subscriptions",
+    localField:"_id",
+    foreignField:"channel",
+    as:"subscribers"
+  }
+},{
+  $lookup:{
+    from:"subscriptions",
+    localField:"_id",
+    foreignField:"subscriber",
+    as:"subscribeTo"
+  }
+},
+{
+  $addFields:{
+    subscriberCount:{
+      $size: "$subscribers"
+    },
+    subscribedtoCount:{
+      $size: "$subscribeTo"
+    },
+    isSubscribed:{
+      $cond:{
+        if:{$in:[req.user?._id,"$subscribers.subscribe"]},
+        then:true,
+        else:false
+      }
+    }
+  }
+},
+{
+  $project:{
+    username:1,
+    subscriberCount:1,
+    subscribedtoCount:1,
+    isSubscribed:1,
+    avatar:1,
+    coverImage:1,
+    email:1
+  }
+}
+])
+if(!channel?.length){
+  throw new apiError(404,"Channel not found")
+}
+
+return res
+.status(200)
+.json(new apiResponse(200,
+  channel[0],"feteched User info Successfully"
+))
+
+
+})
 //exporting the functions or methods
 export {
   loginUser,
@@ -401,5 +474,6 @@ export {
   currentUser,
   updateAccountDetails,
   updateavatar,
-  updatCoverImage
+  updatCoverImage,
+  channelDetails
 }

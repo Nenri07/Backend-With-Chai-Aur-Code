3ff9145418d76c2fd07b177e80f70b7f45012417
Nenri07 <umarbashir2002@gmail.com>
2025-05-10 04:18:59 +0500

    feat: implement getVideoById and updateVideo functions with error handling

--- src/controller/video.controller.js
+++ src/controller/video.controller.js
@@ -104,16 +104,142 @@ const publishAVideo = asyncHandler(async (req, res) => {
     }
 
 })
-
+// done
 const getVideoById = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
+  try {
+      const { videoId } = req.params
+      //get video id form the url through request.params
+      //search video by id from model 
+      //return response
+  
+      if(!videoId){
+          throw new apiError(
+              400,
+              "error while reciving data "
+          )
+      }
+      const video= await Video.findById(videoId)
+      if(!video){
+          throw new apiError(
+              402,
+              "error while fetching data from server try again later"
+          )
+      }
+  
+      return res
+      .status(200)
+      .json(
+          new apiResponse(
+              200,
+              {video},
+              "video fetched successfully"
+          )
+      )
+  
+  } catch (error) {
+    throw new apiError(
+        500,
+        error?.message
+        || "Server Side Request failed Sorry for this time try again later if error presist contact on umer@webape.co"
+    )
+    
+  }
+
+
+    
     //TODO: get video by id
 
 })
-
+//done
 const updateVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
     //TODO: update video details like title, description, thumbnail
+    //first get id
+    //find video by id
+    //check login user is video owner or not
+    //get title and description by req.body
+    //get thumbnail from req.files.path
+    //upload thubumbnail on cloudinary and get cloudinary path by .url
+    // update the document by model.findbyIdandupdate(id,{},{new:true} for modified document to be retuned)
+
+ try {
+       const { videoId } = req.params
+       if(!videoId){
+           throw new apiError(
+               400,
+               "client side error"
+           )
+       }
+       const videotoUpdate=await Video.findById(videoId)
+       
+       const user= req.user?._id
+      
+       
+       if(!(videotoUpdate.Owner!==user)){//
+           throw new apiError(200,"You are not authorized to update this")
+       } 
+   
+       const {title, description}=req.body
+       if(!(title || description)){
+           throw new apiError(
+               400,
+               "please give title and description"
+           )
+       }
+   
+       const newthumbnail=req.file?.path
+       
+       if(!newthumbnail){
+           throw new apiError(
+               400,
+               "please give thumbnail"
+           )
+       }
+   
+       const updatedurl=await uploadOnCloudinary(newthumbnail)
+       if(!updatedurl.url){
+           throw new apiError(
+               400,
+               "there is an error while uploading thumbnail to server . try again later"
+           )
+       }
+   
+       const updatedvideo=await Video.findByIdAndUpdate(videoId,{
+           Title:title,
+           Description:description,
+           thumbnail:updatedurl.url
+       },
+   {
+       new:true
+   })
+   
+   if(!updatedvideo){
+       throw new apiError(400,
+           "Error in the server updation"
+       )
+   }
+   
+   return res
+   
+   .status(
+       200
+   )
+   .json(
+       new apiResponse(200,
+           updatedvideo,
+           "suceessfully updated"
+       )
+   )
+      
+ } catch (error) {
+    throw new apiError(
+        502,
+        error?.message||"internal server error contact devs"
+    )
+ } 
+
+
+
+
 
 })
 
